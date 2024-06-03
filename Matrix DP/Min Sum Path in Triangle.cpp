476071aab8e00ca22edc4bd95a78c315c3485f8b
Siddhant Rohila <150060049+paneerpyaz@users.noreply.github.com>
2024-06-03 23:43:12 +0530

    Create Min Sum Path in Triangle.cpp

---
+++ Matrix DP/Min Sum Path in Triangle.cpp
@@ -0,0 +1,16 @@
+int Solution::minimumTotal(vector<vector<int> > &a) {
+    int n = a.size();
+    vector<vector<int>>dp(n+1,vector<int>(n+1,1e9));
+    dp[0][0] = a[0][0];
+    for(int i = 1 ; i < n ; i++){
+        for(int j = 0 ; j < i+1 ; j++){
+            if(j<i) dp[i][j] = dp[i-1][j]+a[i][j];
+            if(j>=1) dp[i][j]=min(dp[i][j],dp[i-1][j-1]+a[i][j]);
+        }
+    }
+    int ans = 1e9;
+    for(int j = 0 ; j < n ; j++){
+        ans = min(ans,dp[n-1][j]);
+    }
+    return ans;
+}
